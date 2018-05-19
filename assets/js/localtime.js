8ff7b8fa296712dea52cbd72bd9cd2bf38e24fd2
Sneha Dama <snehadama@Snehas-Air.lan>
2018-05-19 18:26:19 -0500

    Added calendar

--- assets/js/localtime.js
+++ assets/js/localtime.js
@@ -1,2 +1,27 @@
-$("#loginModal").on("click", function (event) {
-    event.preventDefault();
+// alert("hi");
+
+// app.post('/logout', (req, res) => {
+//     req.session.destroy();
+//     res.redirect('/');
+//   });
+
+//   $('input[name="dates"]').daterangepicker();
+
+//   $(function() {
+//     $('input[name="daterange"]').daterangepicker({
+//       opens: 'left'
+//     }, function(start, end, label) {
+//       console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+//     });
+//   });
+
+  $(function () {
+    $('input[name="daterange"]').daterangepicker({
+      "showWeekNumbers": true,
+    });
+  });
+  
+  $('#duration').on('apply.daterangepicker', function (ev, picker) {
+    alert('apply clicked!');
+  });
+  
